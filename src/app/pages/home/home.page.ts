15cd47d28e994d0bba810b66866f60a4c60bee44
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-11-23 21:46:24 -0500

    correcion

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
 
-  openAR(){
-
+  openAR() {
+    this.router.navigate(['/ar-viewer']);
   }
 }
