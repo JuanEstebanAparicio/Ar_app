75682d36cf4aae00a5cb8da079ad2b26f1ce66ce
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-11-23 22:02:38 -0500

    camara funcionando en pc pero no en celular

--- src/app/pages/ar-viewer/ar-viewer.page.ts
+++ src/app/pages/ar-viewer/ar-viewer.page.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-ar-viewer',
   templateUrl: './ar-viewer.page.html',
   styleUrls: ['./ar-viewer.page.scss'],
   standalone: false,
 })
-export class ArViewerPage implements OnInit {
+export class ArViewerPage {
+  arSceneUrl: SafeResourceUrl;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private sanitizer: DomSanitizer) {
+    this.arSceneUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/ar/scene.html');
   }
 
 }
