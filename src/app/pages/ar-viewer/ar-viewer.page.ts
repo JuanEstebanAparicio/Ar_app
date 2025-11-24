1a402cefbf05e8fdde11f9d0d7a3ed188511e723
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-11-23 21:44:51 -0500

    htmt css y metodo vacio

--- src/app/pages/ar-viewer/ar-viewer.page.ts
+++ src/app/pages/ar-viewer/ar-viewer.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-ar-viewer',
   templateUrl: './ar-viewer.page.html',
   styleUrls: ['./ar-viewer.page.scss'],
+  standalone: false,
 })
 export class ArViewerPage implements OnInit {
 
